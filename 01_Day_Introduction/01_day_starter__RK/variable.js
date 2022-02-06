bc1f8abc8769ade109f04a44c070581ad2dea218
Rudransh-K <rudranshkulkarniofficial@gmail.com>
2022-02-06 22:52:39 +0530

    Day 1 completed with RK updates
    Adding my take to this 30 Day material with extra documentation and comments and other resources

---
+++ 01_Day_Introduction/01_day_starter__RK/variable.js
@@ -0,0 +1,52 @@
+// // Declaring different variables of different data types
+
+// let firstName = 'Asabeneh'    // first name of a person
+// let lastName = 'Yetayeh'      // last name of a person
+// let country = 'Finland'       // country
+// let city = 'Helsinki'         // capital city
+// let age = 100                 // age in years
+// let isMarried = true
+
+// // Declaring variables with number values
+
+// const gravity = 9.81         // earth gravity  in m/s2
+// const boilingPoint = 100     // water boiling point, temperature in oC
+// const PI = 3.14              // geometrical constant
+
+// // Variables can also be declaring in one line separated by comma
+
+// let name = 'Asabeneh',     //name of a person
+//   job = 'teacher',
+//   live = 'Finland'
+
+
+let name = 'Rudransh Kulkarni'
+let country = 'India'
+let age = 19
+let isMarried = false
+
+const iq = 5000
+
+let song = 'happy',
+    music = 'happier',
+    vocals = 'happiest'
+
+var friendName = 'friend'
+
+// const and let have the same functionality but const cannot be updated
+// let variables can be updated but cannot be reassigned using let
+
+// var is the oldest way of declaring variables
+// var is global scope or function scope as it is hoisted to be the first thing to be executed and therefore
+// will not be an error even if it is used before it is declared in the code
+
+// let and const are block-scoped and can only be used in that block
+// the same variable identifiers can be reassigned in another block
+
+//Users cannot change the properties of the const object, but they can change the value of properties of the const object.
+
+
+
+
+//Other details are explained well here:
+// https://www.geeksforgeeks.org/difference-between-var-let-and-const-keywords-in-javascript/
